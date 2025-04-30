a93f9aa32dce2de32a193e5443a3ef58fe54ec81
Ryuji <hi_ryuji@naver.com>
2025-04-30 15:47:38 +0900

    fix: ~chats/page.tsx 로직 수정

--- src/app/(route)/chats/[id]/page.tsx
+++ src/app/(route)/chats/[id]/page.tsx
@@ -1,10 +1,19 @@
 import { ChatRoom } from '@/components/page/chats/ChatRoom';
 
-export default function ChatRoomPage({ params }: { params: { id: string } }) {
+interface PageProps {
+  params: Promise<{
+    id: string;
+  }>;
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+export default async function ChatRoomPage({ params }: PageProps) {
+  const { id } = await params;
+
   return (
     <main className="container mx-auto">
       <h1 className="text-2xl font-bold my-4">채팅방</h1>
-      <ChatRoom chatRoomId={params.id} />
+      <ChatRoom chatRoomId={id} />
     </main>
   );
 }
